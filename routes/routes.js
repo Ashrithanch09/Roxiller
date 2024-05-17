1b7610f0a730e9df9d2035965bbafdc8a24839b0
Harsh Yadav <harshyadav.hy20@gmail.com>
2024-05-17 15:48:37 +0530

    added all apis and endpoints

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+// import express from 'express';
+
+//  import {data,products,transactions,statistics,combined} from '../controller/controller.js';
+
+// const router = express.Router();
+
+// router.get('/api/data',data);
+// router.get('/api/products',products);
+// router.get('/api/transactions',transactions);
+// router.get('/api/statistics',statistics);
+// router.get('/api/combined',combined);
+
+// module.exports = router;
